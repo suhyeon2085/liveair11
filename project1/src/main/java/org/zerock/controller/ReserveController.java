75d39174ae8f5af0fe1a520003f8564082a6ce36
GRCOMNE <GRCOMNE@DESKTOP-41EUKUB>
2025-05-30 15:44:27 +0900

    0530 출장 서비스 예약 페이지

---
+++ project1/src/main/java/org/zerock/controller/ReserveController.java
@@ -0,0 +1,27 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/reserve/*")
+@AllArgsConstructor
+public class ReserveController {
+
+	// 조회
+	
+	
+	// 삽입
+	
+	
+	// 수정
+	
+	
+	// 삭제
+	
+	
+}
